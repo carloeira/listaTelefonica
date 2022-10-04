027e71d777db6376fbb4cd917decea13034cfcba
n3vill <112698218+carloeira@users.noreply.github.com>
2022-10-03 21:52:48 -0300

    Ajustando tabela para localhost 8001

--- eventos.js
+++ eventos.js
@@ -1,37 +1,69 @@
-let contatos = [
-    ['Carlos', '85 999990000', 'Fortaleza'],
-    ['André', '85 999992222', 'Fortaleza'],
-    ['Adrian', '85 999991111', 'Caucaia'],
-];
+const API_URL = 'http://localhost:8001'
 
 function novo() {
     paginaNovo.style.display = 'block';
     listaDeContatos.style.display = 'none';
 }
 
-
 function listar() {
     listaDeContatos.style.display = 'block';
     paginaNovo.style.display = 'none';
 
-    limpaLista();
-    contatos.forEach(function (cadaContato) {
-        listaDeContatos.innerHTML += `
-            <tr>
-                <td>${cadaContato[0]}</td>
-                <td>${cadaContato[0]}</td>
-                <td>${cadaContato[0]}</td>
-            </tr>
-        `;
-    });
-}
+    fetch(API_URL+'/contatos/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 
-function limpaLista() {
-    listaDeContatos.innerHTML = '';
+        .then( (resposta) => resposta.json())
+        .then( () => atualizarLista())
 }
 
 function salvar() {
     event.preventDefault();
-    contatos.push([`${nome.value}`, `${telefone.value}`, `${cidade.value}`]);
+
+    let dados = {
+        nome: nome.value,
+        telefone: telefone.value,
+        cidade: cidade.value,
+    };
+
+    fetch(API_URL+'/contatos/', {
+        method: 'POST',
+        body: JSON.stringify(dados),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+        .then( (resposta) => resposta.json())
+        .then()
+
+    paginaNovo.reset();
     listar();
 }
+
+function atualizarLista() {
+    listaDeContatos.innerHTML = '';
+
+    fetch(API_URL+'/contatos/')
+    .then(function(resposta) {
+        return resposta.json();
+    })
+    .then(function (lista) {
+        lista.forEach(function (cadaContato) {
+            listaDeContatos.innerHTML += `
+                <tr>
+                    <td>${cadaContato.nome}</td>
+                    <td>${cadaContato.telefone}</td>
+                    <td>${cadaContato.cidade}</td>
+                    <td>${cadaContato.id}</td>
+                </tr>
+                
+            `
+        })
+    })
+}
+
+atualizarLista();
